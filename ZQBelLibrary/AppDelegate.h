bfdaa5e034801d17892948f927ead385649e69e3
李志权 <lizhiquan@lizhiquandeiMac.local>
2017-06-21 17:23:10 +0800

    Initial Commit

---
+++ ZQBelLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQBelLibrary
+//
+//  Created by 李志权 on 2017/6/21.
+//  Copyright © 2017年 李志权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZQBelLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZQBelLibrary
+//
+//  Created by 李志权 on 2017/6/21.
+//  Copyright © 2017年 李志权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
